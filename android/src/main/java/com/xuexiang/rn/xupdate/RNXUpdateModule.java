d424b5e4df0e6b2e904853af409e9badadde438c
suxue-mac <suxuewb@gmail.com>
2023-11-02 12:16:29 +0800

    fix EventEmitter warning

--- android/src/main/java/com/xuexiang/rn/xupdate/RNXUpdateModule.java
+++ android/src/main/java/com/xuexiang/rn/xupdate/RNXUpdateModule.java
@@ -282,5 +282,14 @@ public class RNXUpdateModule extends ReactContextBaseJavaModule {
         }
     }
 
+    @ReactMethod
+    public void addListener(String eventName) {
+        // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+    @ReactMethod
+    public void removeListeners(Integer count) {
+        // Keep: Required for RN built in Event Emitter Calls.
+    }
 
 }
